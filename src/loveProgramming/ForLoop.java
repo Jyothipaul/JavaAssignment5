45e96ca480344fed8e3d3541620559d45f663beb
jyothipaul@yahoo.com <jyothipaul@yahoo.com>
2019-01-06 00:12:06 +0000

    Java Assignment 5

---
+++ src/loveProgramming/ForLoop.java
@@ -0,0 +1,12 @@
+package loveProgramming;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+        System.out.println("Starting With 50");
+        for (int i = 50; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
